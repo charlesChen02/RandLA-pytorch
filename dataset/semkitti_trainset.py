84f625164161816e9ef29fd97bed1882d4b5e3d0
huixiancheng <1552040046@qq.com>
2022-08-26 16:01:26 +0800

    Update

--- dataset/semkitti_trainset.py
+++ dataset/semkitti_trainset.py
@@ -94,6 +94,29 @@ class SemanticKITTI(torch_data.Dataset):
 
         return input_list
 
+    def tf_map_baf_lac(self, batch_pc, batch_label, batch_pc_idx, batch_cloud_idx):
+        features = batch_pc
+        input_points = []
+        input_neighbors = []
+        input_pools = []
+        input_up_samples = []
+
+        for i in range(cfg.num_layers):
+            neighbour_idx = DP.knn_search(batch_pc, batch_pc, cfg.k_n)
+            sub_points = batch_pc[:, :batch_pc.shape[1] // cfg.sub_sampling_ratio[i], :]
+            pool_i = neighbour_idx[:, :batch_pc.shape[1] // cfg.sub_sampling_ratio[i], :]
+            up_i = DP.knn_search(sub_points, batch_pc, 1)
+            input_points.append(batch_pc)
+            input_neighbors.append(neighbour_idx)
+            input_pools.append(pool_i)
+            input_up_samples.append(up_i)
+            batch_pc = sub_points
+
+        batch_graph_idx = DP.knn_search(input_points[0], input_points[0], cfg.k_n)
+        input_list = input_points + input_neighbors + input_pools + input_up_samples
+        input_list += [features, batch_label, batch_pc_idx, batch_cloud_idx]
+        return input_list, batch_graph_idx
+
     def collate_fn(self, batch):
 
         selected_pc, selected_labels, selected_idx, cloud_ind = [], [], [], []
@@ -126,5 +149,42 @@ class SemanticKITTI(torch_data.Dataset):
             inputs['interp_idx'].append(torch.from_numpy(tmp).long())
         inputs['features'] = torch.from_numpy(flat_inputs[4 * num_layers]).transpose(1, 2).float()
         inputs['labels'] = torch.from_numpy(flat_inputs[4 * num_layers + 1]).long()
+
+        return inputs
+
+    def collate_fn_baf_lac(self, batch):
+
+        selected_pc, selected_labels, selected_idx, cloud_ind = [], [], [], []
+        for i in range(len(batch)):
+            selected_pc.append(batch[i][0])
+            selected_labels.append(batch[i][1])
+            selected_idx.append(batch[i][2])
+            cloud_ind.append(batch[i][3])
+
+        selected_pc = np.stack(selected_pc)
+        selected_labels = np.stack(selected_labels)
+        selected_idx = np.stack(selected_idx)
+        cloud_ind = np.stack(cloud_ind)
+
+        flat_inputs, batch_graph_idx = self.tf_map_baf_lac(selected_pc, selected_labels, selected_idx, cloud_ind)
+
+        num_layers = cfg.num_layers
+        inputs = {}
+        inputs['xyz'] = []
+        for tmp in flat_inputs[:num_layers]:
+            inputs['xyz'].append(torch.from_numpy(tmp).float())
+        inputs['neigh_idx'] = []
+        for tmp in flat_inputs[num_layers: 2 * num_layers]:
+            inputs['neigh_idx'].append(torch.from_numpy(tmp).long())
+        inputs['sub_idx'] = []
+        for tmp in flat_inputs[2 * num_layers:3 * num_layers]:
+            inputs['sub_idx'].append(torch.from_numpy(tmp).long())
+        inputs['interp_idx'] = []
+        for tmp in flat_inputs[3 * num_layers:4 * num_layers]:
+            inputs['interp_idx'].append(torch.from_numpy(tmp).long())
+        inputs['features'] = torch.from_numpy(flat_inputs[4 * num_layers]).transpose(1, 2).float()
+        inputs['labels'] = torch.from_numpy(flat_inputs[4 * num_layers + 1]).long()
+
+        inputs['graph_idx'] = torch.from_numpy(batch_graph_idx).long()
 
         return inputs
